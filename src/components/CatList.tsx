b6fc196296cfaf345607ff83b00595f04a8872f9
Michael Lyon <mlyon360@gmail.com>
2024-07-24 18:55:49 -0600

    add delete cat button

--- src/components/CatList.tsx
+++ src/components/CatList.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import useCatList from "../hooks/useCatList";
 import CatListCard from "./CatListCard";
 
 const CatList = () => {
   const catListQuery = useCatList();
+  const [hiddenCats, setHiddenCats] = useState<string[]>([]);
 
   if (catListQuery.isLoading) {
     return <p>loading...</p>;
   }
 
+  const cats = catListQuery.data?.filter((cat) => !hiddenCats.includes(cat.id));
+
   return (
     <div className="max-w-96 mx-auto mt-12">
-      {catListQuery.data?.map((cat) => (
-        <CatListCard key={cat.id} cat={cat} />
+      {cats?.map((cat) => (
+        <CatListCard
+          key={cat.id}
+          cat={cat}
+          onDelete={() => setHiddenCats([...hiddenCats, cat.id])}
+        />
       ))}
     </div>
   );
--- src/components/CatListCard.tsx
+++ src/components/CatListCard.tsx
@@ -3,11 +3,12 @@ import { CatSchema } from "../hooks/useCatList";
 
 type CatListCardProps = {
   cat: z.infer<typeof CatSchema>;
+  onDelete: () => void;
 };
 
-const CatListCard = ({ cat }: CatListCardProps) => {
+const CatListCard = ({ cat, onDelete }: CatListCardProps) => {
   return (
-    <div className="mb-8 text-center">
+    <div className="mb-8 text-center relative">
       <img
         className="object-cover rounded-md mb-1"
         src={cat.url}
@@ -18,6 +19,24 @@ const CatListCard = ({ cat }: CatListCardProps) => {
       <p className="text-lg">
         {cat.breeds.map((breed) => breed.name).join(", ")}
       </p>
+
+      <div className="p-0.5 bg-white shadow-lg absolute top-4 right-4 leading-none rounded-full">
+        <button className="size-6" onClick={onDelete}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-x"
+          >
+            <path d="M18 6 6 18" />
+            <path d="m6 6 12 12" />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
